68848e2583809336af971f848d807cea986b6e15
Vanessa Martin <vanessa.s.martin96@gmail.com>
2024-05-17 13:17:17 -0700

    Following instructions - intial setup

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  )
+}
+
+export default Page
